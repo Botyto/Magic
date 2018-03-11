764fbefc8c85f0f34cf4e634dc7cd73ab2b57fa6
Botyto <biserkrustev.botyto@gmail.com>
2018-03-11 11:33:43 +0200

    [init] Add editor scripts

---
+++ Assets/Editor/EnergyManifestationEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EnergyManifestation))]
+public class EnergyManifestationEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (Application.isPlaying)
+        {
+            if (GUILayout.Button(new GUIContent("Smash")))
+            {
+                (target as EnergyManifestation).Smash();
+            }
+        }
+    }
+}
---
+++ Assets/Editor/SpellDescriptorEditor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SpellDescriptor))]
+public class SpellDescsriptorEditor : Editor
+{
+    public void OnSceneGUI()
+    {
+        GUILayout.Label("GUID: " + (target as SpellDescriptor).ToString());
+        DrawDefaultInspector();
+    }
+}
