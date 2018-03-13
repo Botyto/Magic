dc547bb4bd19aed4865080fbe2a3c02af8fe20da
Botyto <biserkrustev.botyto@gmail.com>
2018-03-13 08:42:25 +0200

    Enemy always looks at player in test scene

---
+++ Assets/Samples/LookTowardsObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class LookTowardsObject : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Util.Destroy(this);
+            return;
+        }
+            
+        transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position, Vector3.up);
+    }
+}
