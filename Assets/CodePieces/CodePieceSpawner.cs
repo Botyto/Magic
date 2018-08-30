47fd251660901af9f30f024e27b628357ef774b9
Botyto <biserkrustev.botyto@gmail.com>
2018-08-30 16:49:22 +0300

    Code piece spawn buttons

---
+++ Assets/CodePieces/CodePieceSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CodePieceSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public void Spawn()
+    {
+        var container = FindObjectOfType<CodeContainer>();
+        var crt = container.transform as RectTransform;
+        var csize = crt.rect.size;
+
+        var newPiece = Instantiate(prefab, crt);
+
+        var rt = prefab.transform as RectTransform;
+        var size = rt.rect.size;
+        //rt.position = (crt.sizeDelta / 2) - (size / 2);
+    }
+}
