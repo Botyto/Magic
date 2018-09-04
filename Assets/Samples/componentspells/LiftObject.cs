ea18d3a4329a5f3dfa6363ab6d18e95313466829
Botyto <biserkrustev.botyto@gmail.com>
2018-09-04 09:31:57 +0300

    Create 'Playground' scene

---
+++ Assets/Samples/componentspells/LiftObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class LiftObject : InstantSpellComponent
+{
+    public override void Cast()
+    {
+        int handle;
+        if (!Try(ManifestEnergyAndFocusAbsolute(param.level * 50, TargetPosition + Vector3.up * 3, out handle)))
+        {
+            Cancel();
+            return;
+        }
+
+        if (!Try(CreateElasticConnection(handle, target, param.level * 200)))
+        {
+            Cancel();
+            return;
+        }
+
+        if (!Try(ApplyForce(handle, Vector3.up * 200 * param.level, ForceMode.Impulse)))
+        {
+            Cancel();
+            return;
+        }
+
+        Finish();
+    }
+}
